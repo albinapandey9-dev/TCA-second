275b95ebac2cc2df40e64f1fdb1d418ea1a079d9
Albina pandey <pandeyalbina960@gmail.com>
2026-08-30 21:13:32 +0545

    Update JavaScript functionality and Week 1 page

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-alert('script loaded');
-alert('hello');
-alert('my');
-alert('name');
-alert('is');
-alert('albina');
+
+
+
+function scriptloaded(){
+      alert('scriptloaded');
+}
+function addition()
+{
+    var num1= 5;
+    var num2= 10;
+    var num3= num1 + num2;
+    alert(num3);
+}
+addition();
