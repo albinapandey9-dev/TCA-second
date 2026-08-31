c5b011e489714a02420b17d143e506c5030040d2
Albina pandey <pandeyalbina960@gmail.com>
2026-08-31 08:58:01 +0545

    Add JavaScript files and selecting page

---
+++ main.js
@@ -0,0 +1,5 @@
+ function myLoadFunction() {
+ var a=document.getElementById('main');
+a.firstChild.nodeValue = 'HELLO nepal';
+ }
+ document.addEventListener('DOMContentLoaded',myLoadFunction);
---
+++ major.js
@@ -0,0 +1,8 @@
+function change(){
+
+    var element = document.getElementById('pageheading2');
+    // alert(element);
+    element.firstChild.nodeValue = 'New Heading';
+}
+
+document.addEventListener('click',change);
